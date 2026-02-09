3d56591fed190f4251d5b5d1710383eca99c10c7
Amit Weitzner <amit@timbr.ai>
2026-02-09 16:32:24 +0200

    Fix test for multiple ontologies

--- tests/integration/test_langchain_chains.py
+++ tests/integration/test_langchain_chains.py
@@ -445,7 +445,7 @@ class TestGenerateAnswerChain:
         assert isinstance(result["rows"], list), "'rows' should be a list"
         assert result["sql"], "SQL should be present in the result"
         assert "CUSTOMER" in result["sql"].upper(), "SQL should contain 'CUSTOMER'"
-        assert list(result["rows"][0].values())[0] == 454 or list(result["rows"][0].values())[0] == 20652, "Number of rows should not exceed max_limit"
+        assert list(result["rows"][0].values())[0] == 781 or list(result["rows"][0].values())[0] == 20652, "Number of rows should not exceed max_limit"
         inputs = {
             "prompt": "count number of patients",
         }
